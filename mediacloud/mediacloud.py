86b181aa777687cb564269ee3331d2b8bc152f39
Tilman Kerl <tilman.kerl@eatclever.de>
2019-12-07 13:43:44 +0100

    rename

--- mediacloud/mediacloud.py
+++ mediacloud/mediacloud.py
@@ -2,7 +2,7 @@
 import mediacloud.api
 
 API_KEY = "69607f50090e4953805652c6e7591e9b21e02a7c2b4e58503d4ec44e3b2d26cc"
-DOCS = "https://github.com/berkmancenter/mediacloud/blob/master/doc/api_2_0_spec/api_2_0_spec.md#authentication"
+DOCS = "https://github.com/berkmancenter/mediacloud/blob/master/doc/api_2_0_spec/api_2_0_spec.md"
 
 mc = mediacloud.api.MediaCloud(API_KEY)
 res1 = mc.storyCount('impeachment AND president AND tags_id_media:58722749', 'publish_date:[NOW-1YEAR TO NOW]')
